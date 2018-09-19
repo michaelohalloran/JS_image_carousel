ad57142725edcd6be29a406d83fcdee32a565328
MJ <kipsang20323@gmail.com>
2018-09-19 17:20:39 -0600

    fix image centering

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const defaultImg = "http://via.placeholder.com/350x150/ff0000/fff?text=Default";
 let imagePlace = 0;
 // let images = ['1','2','3','4','5','6','7'];
-let images = ['brainardRoad.jpg','brainardMtn.jpg','evans_clouds.jpg','evans.jpg','fireSky.jpg','sunset.jpg','ward.jpg'];
+let images = ['brainardRoad.jpg','brainardMtn.jpg','evans_clouds.jpg','fireSky.jpg','sunset.jpg','ward.jpg'];
 // let imageColors = ['000','111','222','333','FF0000', '00FF00', '0000FF'];
 
 //run fn right when window loads
@@ -18,7 +18,7 @@ function swapImages() {
      let myImage = document.getElementById('myImage');
      //we will pick the image src color to be our current array position
     //  myImage.src = `http://via.placeholder.com/350x150/${imageColors[imagePlace]}/fff?text=Image${images[imagePlace]}`;
-    console.log('before starting swapImages, imagePlace is', imagePlace);
+    // console.log('before starting swapImages, imagePlace is', imagePlace);
     myImage.src = images[imagePlace];
     //increment what place we're at in our image array
     //when we hit the end, reset to 0
